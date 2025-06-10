f4ad6057385350092737d2ddf168d97572ab152b
Muhammad Shakeel <148995672+Shakeel-Ilyas@users.noreply.github.com>
2025-06-10 20:19:04 +0500

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    firebaseApiKey: "AIzaSyAHGjE1QNq7OnNJh8JLV_6juBhAyF4wfj4",
-    firebaseRealtimeDataBaseApiKey: 'https://angularhttpclient-cb10c-default-rtdb.firebaseio.com/',
-    firebaseSignupApiKey: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
-    firebaseSigninApiKey: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
-}
